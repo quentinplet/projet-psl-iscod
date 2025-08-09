22ca9d30870e5f8ce295fc9adf8d0c7314b21c5a
Quentin Plet <quentin.plet@gmail.com>
2025-08-09 19:26:05 +0200

    Update Dockerfile to use PHP CLI, enhance startup script, and optimize dependency installation

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -9,9 +9,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -23,8 +23,12 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->validateCsrfTokens(
             except: ['webhook/stripe']
-            );
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (\Throwable $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        });
     })->create();
